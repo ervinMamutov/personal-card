f7abff9fbb9534aa0cbd473b2ac0566a87e0e191
ervinMamutov <ervin.news@gmail.com>
2023-09-18 16:52:15 +0200

    added Loader0

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import Card from './components/Card';
 import Header from './components/Header';
 import PersonasCard from './components/PersonasCards';
 import getUsersApi from '../Apis/getUsersApi';
-import { useEffect, useState } from 'react';
+import Loader from './components/Loader';
 
 function App() {
   const [userCard, setUserCard] = useState([]);
+  const [isLoader, setIsLoader] = useState(false);
 
   useEffect(() => {
     const getData = async () => {
       const data = await getUsersApi();
-      setUserCard(data);
+      if (!data) {
+        setIsLoader(true);
+      } else {
+        setUserCard(data);
+        setIsLoader(false);
+      }
     };
     getData();
   }, []);
@@ -21,7 +28,7 @@ function App() {
   return (
     <Card>
       <Header />
-      <PersonasCard data={userCard} />
+      {isLoader ? <Loader /> : <PersonasCard data={userCard} />}
     </Card>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,15 @@ import './Header.css';
 import PropTypes from 'prop-types';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="header">
+      <div className="header__container">
+        <div className="title">
+          <h1 className="title-text">Personas Cards</h1>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 Header.propTypes = {};
